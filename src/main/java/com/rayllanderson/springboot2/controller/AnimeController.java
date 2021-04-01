2f30eb33585d820b5862fce28de4e89be7df9af0
Rayllanderson <rayllanderson@gmail.com>
2021-04-01 00:46:30 -0300

    aula 2

--- src/main/java/com/rayllanderson/springboot2/controller/AnimeController.java
+++ src/main/java/com/rayllanderson/springboot2/controller/AnimeController.java
@@ -1,18 +1,27 @@
 package com.rayllanderson.springboot2.controller;
 
 import com.rayllanderson.springboot2.domain.Anime;
+import com.rayllanderson.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/animes")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
 
+    private final DateUtil dateUtil;
+
     @GetMapping
-    public List<Anime> findAll(){
-        return List.of(new Anime("Berserk"), new Anime("Horimiya"));
+    public List<Anime> findAll() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Berserk"), new Anime("Eu não sei exatamente como"));
     }
 }
