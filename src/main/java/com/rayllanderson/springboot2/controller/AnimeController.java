c63c76c6f879c6c51991d3e00be0343b561cfdb3
Rayllanderson <rayllanderson@gmail.com>
2021-04-01 03:34:13 -0300

    post

--- src/main/java/com/rayllanderson/springboot2/controller/AnimeController.java
+++ src/main/java/com/rayllanderson/springboot2/controller/AnimeController.java
@@ -5,11 +5,9 @@ import com.rayllanderson.springboot2.services.AnimeService;
 import com.rayllanderson.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -33,5 +31,10 @@ public class AnimeController {
     public ResponseEntity<Anime> findById(@PathVariable long id) {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(animeService.save(anime));
     }
 }
--- src/main/java/com/rayllanderson/springboot2/services/AnimeService.java
+++ src/main/java/com/rayllanderson/springboot2/services/AnimeService.java
@@ -5,20 +5,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L, "Berserk"), new Anime(2L, "Kaguya sama"));
-    public List<Anime> findAll(){
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Berserk"), new Anime(2L, "Kaguya sama")));
+    }
+
+    public List<Anime> findAll() {
         return animes;
     }
 
-    public Anime findById(long id){
+    public Anime findById(long id) {
         return animes.stream().filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100));
+        animes.add(anime);
+        return anime;
     }
 
 }
