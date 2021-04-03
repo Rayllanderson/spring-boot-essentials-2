e7174d50b219837325cd1e9b3269be96b4a38b45
Rayllanderson <rayllanderson@gmail.com>
2021-04-03 19:59:07 -0300

    RestTemplate PUT and DELETE

--- src/main/java/com/rayllanderson/springboot2/clients/SpringClient.java
+++ src/main/java/com/rayllanderson/springboot2/clients/SpringClient.java
@@ -13,17 +13,20 @@ public class SpringClient {
 
     public static void main(String[] args) {
         final String API_URL = "http://localhost:8080/animes";
+
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity(API_URL + "/{id}", Anime.class, 2);
         log.info(entity);
         Anime anime = new RestTemplate().getForObject(API_URL + "/{id}", Anime.class, 2);
         log.info("-------");
         log.info(anime);
 
+
         log.info("-------");
         ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange(API_URL + "/all", HttpMethod.GET, null,
                 new ParameterizedTypeReference<>() {
         });
         log.info(exchange.getBody());
+
 
         //POST com postForObject
         //        log.info("-------");
@@ -35,9 +38,21 @@ public class SpringClient {
         Anime onePunchMan = Anime.builder().name("One Punch Man").build();
         ResponseEntity<Anime> savedOnePunchMan = new RestTemplate().exchange(API_URL, HttpMethod.POST, new HttpEntity<>(onePunchMan,
                 createJsonHeader()), Anime.class);
-
         log.info("-------");
         log.info(savedOnePunchMan);
+
+
+        Anime animeToBeUpdated = savedOnePunchMan.getBody();
+        animeToBeUpdated.setName("One Punch Man 2");
+        ResponseEntity<Void> onePunchManUpdated = new RestTemplate().exchange(API_URL + "/{id}", HttpMethod.PUT,
+                new HttpEntity<>(animeToBeUpdated, createJsonHeader()), Void.class, animeToBeUpdated.getId());
+        log.info("Updated: {}", onePunchManUpdated);
+
+
+        ResponseEntity<Void> onePunchManDelete = new RestTemplate().exchange(API_URL + "/{id}", HttpMethod.DELETE,
+                null, Void.class, animeToBeUpdated.getId());
+        log.info("Deleted: {}", onePunchManDelete);
+
     }
 
     private static HttpHeaders createJsonHeader() {
