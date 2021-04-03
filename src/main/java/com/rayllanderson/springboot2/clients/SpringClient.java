eda0dd0f4c497128a5de7425160cdcc6af0d49a5
Rayllanderson <rayllanderson@gmail.com>
2021-04-03 19:44:45 -0300

    RestTemplate POST

--- src/main/java/com/rayllanderson/springboot2/clients/SpringClient.java
+++ src/main/java/com/rayllanderson/springboot2/clients/SpringClient.java
@@ -3,8 +3,7 @@ package com.rayllanderson.springboot2.clients;
 import com.rayllanderson.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -13,17 +12,37 @@ import java.util.List;
 public class SpringClient {
 
     public static void main(String[] args) {
-        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
+        final String API_URL = "http://localhost:8080/animes";
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity(API_URL + "/{id}", Anime.class, 2);
         log.info(entity);
-        Anime anime = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
+        Anime anime = new RestTemplate().getForObject(API_URL + "/{id}", Anime.class, 2);
         log.info("-------");
         log.info(anime);
 
         log.info("-------");
-        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null,
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange(API_URL + "/all", HttpMethod.GET, null,
                 new ParameterizedTypeReference<>() {
         });
         log.info(exchange.getBody());
 
+        //POST com postForObject
+        //        log.info("-------");
+        //        Anime nagatoro = Anime.builder().name("Nagotoro").build();
+        //        Anime nagatodoSaved = new RestTemplate().postForObject(API_URL, nagatoro, Anime.class);
+        //        log.info(nagatodoSaved);
+
+        //POST com exchange
+        Anime onePunchMan = Anime.builder().name("One Punch Man").build();
+        ResponseEntity<Anime> savedOnePunchMan = new RestTemplate().exchange(API_URL, HttpMethod.POST, new HttpEntity<>(onePunchMan,
+                createJsonHeader()), Anime.class);
+
+        log.info("-------");
+        log.info(savedOnePunchMan);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
     }
 }
