579863ea21813e9cc13c0837ea623dacd7c7c505
Rayllanderson <rayllanderson@gmail.com>
2021-04-04 21:29:46 -0300

    Maven Profile Para Testes de Integração

--- src/test/java/com/rayllanderson/springboot2/SpringbootEssentials2ApplicationTests.java
+++ src/test/java/com/rayllanderson/springboot2/SpringbootEssentials2ApplicationTests.java
@@ -3,10 +3,10 @@ package com.rayllanderson.springboot2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringbootEssentials2ApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
