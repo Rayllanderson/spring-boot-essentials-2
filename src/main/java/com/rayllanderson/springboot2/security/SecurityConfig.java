7da49904321d02a493f23b3c7fccba27c595eaa2
Rayllanderson <rayllanderson@gmail.com>
2021-04-05 02:54:05 -0300

    Spring Security pt 01 - Autenticação em memória

---
+++ src/main/java/com/rayllanderson/springboot2/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.rayllanderson.springboot2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("ray")
+                .password(getEncoder().encode("123"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("joao")
+                .password(getEncoder().encode("123"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
